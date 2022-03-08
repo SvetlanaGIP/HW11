1a76282b556530fc4771d824b34e022e733ed277
SvetlanaGIP <gip1@acn.su>
2022-03-09 08:32:01 +1000

    Добавлены критические моментыДобавьте файлы проекта.

---
+++ HW11/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11
+{
+    class Program
+    {
+        //Разработать структуру для решения линейного уравнения 0=kx+b.
+        //Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.Для решения уравнения предусмотреть метод Root.
+        //Создать экземпляр разработанной структуры.Осуществить использование экземпляра в программе.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для решения линейного уравнение 0 = kx + b");
+            Console.Write("Введите k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+            LineEq lineEq = new LineEq(k, b);
+            lineEq.Root();
+            Console.ReadKey();
+        }
+        struct LineEq
+        {
+            public double k { get; set; }
+            public double b { get; set; }
+            public LineEq(double k, double b)
+            {
+                k = k;
+                b = b;
+            }
+            public void Root()
+            {
+
+                if (k == 0 && b == 0)
+                {
+                    Console.WriteLine("Уравнение имеет бесконечное количество решени");
+                }
+                else
+                {
+                    if (k == 0)
+                    {
+                        Console.WriteLine("Уравнение не имеет решения"); 
+                    }
+                    else
+                    {
+                        double x = -b / k;
+                        Console.WriteLine("Корень уровнения х = {0}", х);
+                    }
+                }
+            }
+                
+        }
+    }
+}
